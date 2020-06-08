448b90bbbee6ee63aed2fc989734ac0884495de3
teiger212 <eyalteiger@gmail.com>
2020-06-08 19:17:34 +0300

    Starting front end

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useHelloQuery } from './generated/graphql';
 
 function App() {
+  const { data, loading } = useHelloQuery()
+
+  if (loading || !data) {
+    return <div>Loading...</div>
+  }
   return (
-    <h1>hello</h1>
+  <h1>{data.hello}</h1>
   )
 }
 
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,14 @@
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' })
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={ client }>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 )
